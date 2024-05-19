f4dc112125be9c9353ee3efe58dccc1153de6796
Ednilson Araújo <33930720+EdnilsonAraujo@users.noreply.github.com>
2024-05-19 19:03:53 -0300

    Ajuste na mensagem final

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -1,21 +1,22 @@
 import java.util.Scanner;
 
 public class ContaBanco {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o número da conta: ");
+        System.out.print("Por favor, digite o número da conta: ");
         int numeroConta = scanner.nextInt();
-        System.out.print("Digite o número da agência: ");
+        scanner.nextLine();
+        System.out.print("Por favor, digite o número da Agência: ");
         String numeroAgencia = scanner.next();
+        scanner.nextLine();
         System.out.print("Digite seu nome: ");
         String nome = ""; 
-        while(scanner.hasNext()){
-            nome+=scanner.nextLine();
-            }
+        nome+=scanner.nextLine();
         System.out.print("Digite seu saldo: ");
         float saldo = scanner.nextFloat();
 
         scanner.close(); 
-        System.out.println("Hello, World!");
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + numeroAgencia + ". Sua conta é " + numeroConta + " e seu saldo de "+ saldo + " já está disponível para saque.");
     }
 }
